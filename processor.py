a6972ecda5a0128608a305e17c86edb3ed261441
Pun <80048553+thammanant@users.noreply.github.com>
2024-01-09 15:56:33 +0700

    Phase 2: version 1

--- processor.py
+++ processor.py
@@ -1,6 +1,5 @@
 import re
 
-
 def read_lex_file(file_name):
     patterns = {}
     with open(file_name, 'r') as file:
@@ -9,7 +8,6 @@ def read_lex_file(file_name):
                 token_type, pattern = line.strip().split(None, 1)
                 patterns[token_type] = pattern
     return patterns
-
 
 def tokenize(input_string, patterns):
     sorted_patterns = sorted(patterns.items(), key=lambda x: len(x[1]), reverse=True)
@@ -23,11 +21,10 @@ def tokenize(input_string, patterns):
                 break
     return tokens
 
-
 def main():
     lex_file_name = '64011658_64011594.lex'
     input_file_name = 'input.txt'
-    output_file_name = '64011658_64011594.tok'
+    output_file_name = '64011658_64011594.bracket'
 
     token_patterns = read_lex_file(lex_file_name)
 
@@ -36,17 +33,38 @@ def main():
 
     tokenized_input = tokenize(input_string, token_patterns)
 
+    line_number = 1
+    char_position = 1
+    error_messages = []
+
     with open(output_file_name, 'w') as output_file:
+        current_expression = ''
         for token in tokenized_input:
-            if token[0] in {'ADD', 'SUB', 'MUL', 'DIV', 'INT_DIV', 'GT', 'GTE', 'LT', 'LTE', 'EQ', 'NEQ', 'ASSIGN'}:
-                output_file.write(f"{token[1]}/{token[1]} ")
+            if token[0] in {'ADD', 'SUB', 'MUL', 'DIV', 'POW', 'INT_DIV', 'GT', 'GTE', 'LT', 'LTE', 'EQ', 'NEQ', 'ASSIGN'}:
+                current_expression += token[1]
             elif token[0] == 'WS':
-                output_file.write(f"{token[1]}")
+                pass
             else:
-                output_file.write(f"{token[1]}/{token[0]} ")
+                if current_expression:
+                    output_file.write(f"({current_expression}) ")
+                    current_expression = ''
+                if token[0] == 'ERR':
+                    error_messages.append(f"SyntaxError at line {line_number}, pos {char_position}")
+                else:
+                    current_expression += token[1]
+            if token[1] == '\n':
+                if current_expression:
+                    output_file.write(f"({current_expression})\n")
+                    current_expression = ''
+                line_number += 1
+                char_position = 1
+            else:
+                char_position += len(token[1])
+
+    with open(output_file_name, 'a') as output_file:
+        output_file.write('\n'.join(error_messages))
 
     print(f"Output written to {output_file_name}")
-
 
 if __name__ == "__main__":
     main()
