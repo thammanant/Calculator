bd3bfd8cef8edf2d10c1739467fbf48808d9b021
Puttiphat U <90830272+Puttiphat-M@users.noreply.github.com>
2024-01-10 19:22:26 +0700

    UPDATE PROCESSOR

--- processor.py
+++ processor.py
@@ -24,6 +24,81 @@ def tokenize(input_string, patterns):
     return tokens
 
 
+def check_op_compare(current_expression, line_number, output_file):
+    if list(current_expression[1].values())[0] in {'ADD', 'SUB', 'MUL', 'DIV', 'INT_DIV'}:
+        if list(current_expression[2].values())[0] in {'INT', 'REAL', 'VAR'}:
+            pass
+        elif list(current_expression[2].values())[0] in {'ADD', 'SUB'}:
+            if list(current_expression[3].values())[0] in {'INT', 'REAL', 'VAR'}:
+                pass
+            else:
+                output_file.write(f"Syntax error at line {line_number}, pos 4")
+        else:
+            output_file.write(f"Syntax error at line {line_number}, pos 3")
+    elif list(current_expression[1].values())[0] in {'GT', 'GTE', 'LT', 'LTE', 'EQ', 'NEQ'}:
+        if list(current_expression[2].values())[0] in {'INT', 'REAL', 'VAR'}:
+            pass
+        elif list(current_expression[2].values())[0] in {'ADD', 'SUB'}:
+            if list(current_expression[3].values())[0] in {'INT', 'REAL', 'VAR'}:
+                pass
+            else:
+                output_file.write(f"Syntax error at line {line_number}, pos 4")
+        else:
+            output_file.write(f"Syntax error at line {line_number}, pos 3")
+    else:
+        output_file.write(f"Syntax error at line {line_number}, pos 2")
+
+
+def varnum(current_expression, line_number, output_file, var, csv_value):
+    if list(current_expression[0].values())[0] in {'VAR'}:
+        if list(current_expression[1].values())[0] in {'ASSIGN'}:
+            if list(current_expression[2].values())[0] in {'INT', 'REAL', 'VAR'}:
+                if list(current_expression[2].values())[0] in 'VAR' and list(current_expression[2].keys())[
+                    0] not in var:  # Eg:x=y
+                    output_file.write(f"Undefined variable {list(current_expression[2].keys())[0]} at line {line_number}, pos 3")
+                elif list(current_expression[0].keys())[0] not in var:  # var assigned
+                    var.append(list(current_expression[0].keys())[0])
+                    csv_value.append(
+                        f"{list(current_expression[0].keys())[0]}, {line_number}, {len(list(current_expression[0].keys())[0])}, {list(current_expression[0].values())[0]}, {list(current_expression[2].keys())[0]}")
+                else:  # var updated
+                    csv_value.append(
+                        f"{list(current_expression[0].keys())[0]}, {line_number}, {len(list(current_expression[0].keys())[0])}, {list(current_expression[0].values())[0]}, {list(current_expression[2].keys())[0]}")
+            elif list(current_expression[2].values())[0] in {'ADD', 'SUB'}:
+                if list(current_expression[3].values())[0] in {'INT', 'REAL', 'VAR'}:
+                    if list(current_expression[3].values())[0] in 'VAR' and list(current_expression[3].keys())[0] not in var:  # Eg:x=-z
+                        output_file.write(f"Undefined variable {list(current_expression[3].keys())[0]} at line {line_number}, pos 4")
+                    elif list(current_expression[0].keys())[0] not in var:  # var assigned
+                        var.append(list(current_expression[0].keys())[0])
+                        csv_value.append(f"{list(current_expression[0].keys())[0]}, {line_number}, {len(list(current_expression[0].keys())[0])}, {list(current_expression[0].values())[0]}, {list(current_expression[2].keys())[0] + list(current_expression[3].keys())[0]}")
+                    else:  # var updated
+                        csv_value.append(f"{list(current_expression[0].keys())[0]}, {line_number}, {len(list(current_expression[0].keys())[0])}, {list(current_expression[0].values())[0]}, {list(current_expression[2].keys())[0] + list(current_expression[3].keys())[0]}")
+                else:  # Eg:x=+*
+                    output_file.write(f"Syntax error at line {line_number}, pos 4")
+            else:
+                output_file.write(f"Syntax error at line {line_number}, pos 3")
+        else:  # var assigned
+            if list(current_expression[0].keys())[0] in var:
+                check_op_compare(current_expression, line_number, output_file)
+            else:  # var not assigned
+                output_file.write(f"Undefined variable {list(current_expression[0].keys())[0]} at line {line_number},pos 1")
+
+    elif list(current_expression[0].values())[0] in {'INT', 'REAL'}:
+        check_op_compare(current_expression, line_number, output_file)
+
+
+def validation(current_expression, line_number, output_file, var, csv_value):
+    if list(current_expression[0].values())[0] in {'ADD', 'SUB', 'INT', 'REAL', 'VAR'}:
+        if list(current_expression[0].values())[0] in {'ADD', 'SUB'}:
+            output_file.write(list(current_expression[0].keys())[0])
+            del current_expression[0]
+            varnum(current_expression, line_number, output_file, var, csv_value)
+        else:
+            varnum(current_expression, line_number, output_file, var, csv_value)
+
+    else:
+        output_file.write(f"Syntax error at line {line_number}, pos 1")
+
+
 def main():
     lex_file_name = '64011658_64011594.lex'
     input_file_name = 'input.txt'
@@ -38,7 +113,6 @@ def main():
     tokenized_input = tokenize(input_string, token_patterns)
 
     line_number = 1
-    char_position = 1
 
     with open(output_file_name, 'w') as output_file:
         for token in tokenized_input:
@@ -51,60 +125,26 @@ def main():
 
     print(f"Output written to {output_file_name}")
 
-    new_line = False
-    var = []
-
     with open(outputbracket_file_name, 'w') as output_file:
-        current_expression = ''
-        prev_token_type = None
+        current_expression = []
+        csv_value = []
+        var = []
 
-        for i, token in enumerate(tokenized_input):
-            # Use i directly to get the next token
-            next_token = tokenized_input[i + 1] if i + 1 < len(tokenized_input) else None
-            if new_line and token[1] != '\n':
+        for token in tokenized_input:
+            if token[1] == '\n':
+                if len(current_expression) >= 3:
+                    validation(current_expression, line_number, output_file, var, csv_value)
+                else:
+                    output_file.write(f"Syntax error at line {line_number}, pos {len(current_expression)}")
+                output_file.write("\n")
+                current_expression = []
+                line_number += 1
+            elif token[0] == 'WS':
                 continue
             else:
-                new_line = False
-
-            if token[0] in {'ADD', 'SUB', 'MUL', 'DIV', 'POW', 'INT_DIV', 'GT', 'GTE', 'LT', 'LTE', 'EQ', 'NEQ'}:
-                current_expression += token[1]
-            elif token[0] in {'ASSIGN'}:
-                current_expression += token[1]
-            elif token[0] == 'WS':
-                pass
-            elif token[0] in {'VAR', 'REAL', 'INT'}:  # operand
-                if token[0] == 'VAR' and token[1] not in var:
-                    if next_token[0] != 'ASSIGN':
-                        output_file.write(f"Undefined variable {token[1]} at line {line_number}, pos {char_position}")
-                        current_expression = ''
-                        new_line = True
-                    else:
-                        var.append(token[1])
-
-                if prev_token_type in {'VAR', 'REAL', 'INT'}:
-                    output_file.write(f"Syntax error at line {line_number}, pos {char_position}")
-                    current_expression = ''
-                    new_line = True
-                else:
-                    current_expression += token[1]
-            else:             # error
-                if token[0] == 'ERR':           # not sure
-                    output_file.write(f"Syntax error at line {line_number}, pos {char_position}")
-                    current_expression = ''
-                    new_line = True
-
-            prev_token_type = token[0]
-
-            if token[1] == '\n':
-                if current_expression:
-                    output_file.write(f"({current_expression})\n")
-                    current_expression = ''
-                else:
-                    output_file.write(f"\n")
-                line_number += 1
-                char_position = 1
-            else:
-                char_position += len(token[1])
+                dict = {token[1]: token[0]}
+                current_expression.append(dict)
+        print(csv_value)
 
     print(f"Output written to {outputbracket_file_name}")
 
